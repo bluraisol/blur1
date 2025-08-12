bb0021e80f75c00542e0723bde9fa8e958df51dd
bluraisol <megatroncrazy@onionmail.com>
2025-08-12 05:09:23 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import HomePage from './pages/HomePage';
 import PricingPage from './pages/PricingPage';
 import TokenomicsPage from './pages/TokenomicsPage';
 import ConnectPage from './pages/ConnectPage';
-import WhitepaperPage from './pages/WhitepaperPage';
 
 function App() {
   const [activeSection, setActiveSection] = useState('home');
@@ -54,7 +53,6 @@ function App() {
           <Route path="/pricing" element={<PricingPage />} />
           <Route path="/tokenomics" element={<TokenomicsPage />} />
           <Route path="/connect" element={<ConnectPage />} />
-          <Route path="/whitepaper" element={<WhitepaperPage />} />
         </Routes>
         
         <Footer />
